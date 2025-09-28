e775a02b9b2ff6937900d280717549d7bba84d53
nikola-cao <cao.nikola.a@gmail.com>
2025-09-27 20:39:36 -0400

    initial

---
+++ ers/ersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ersApp.swift
+//  ers
+//
+//  Created by Nikola Cao on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
